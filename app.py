03c5a708ca9943d89d80a5ccd8dfd3d989bc720e
Yan Ambrosio Gralha <yan.ambrosio.gralha@gmail.com>
2026-02-26 16:40:09 -0300

    Adição do sistema de login

--- app.py
+++ app.py
@@ -10,16 +10,14 @@ JSON = "users.json"
 def index():
     return render_template("index.html")
 
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
         nome = request.form.get("nome", "")#Faço o get no form pelo name "nome"
         idade = request.form.get("idade", "")#Faço o get no form pelo name "idade"
-        # Passa as variáveis via URL
         
         if nome != "" and idade != "":
-           
-                
             # Se o arquivo não existir, cria com lista vazia
             if not os.path.exists(JSON):
                 with open(JSON, "w", encoding="utf-8") as f:
@@ -30,7 +28,7 @@ def register():
                 usuarios = json.load(f)
 
             # Gera ID único
-            if usuarios == True:
+            if usuarios:
                 novo_id = max(u["id"] for u in usuarios) + 1
             else:
                 novo_id = 1
@@ -48,10 +46,26 @@ def register():
             with open(JSON, "w", encoding="utf-8") as f:
                 json.dump(usuarios, f, ensure_ascii=False, indent=4)
 
-            return redirect(url_for("home", nome=nome, idade=idade))
+            return redirect(url_for("login"))
 
     return render_template("register.html")#se for GET, ou seja, apenas abrir a página, mostra o formulário HTML
     
+@app.route("/login", methods =["GET", "POST"])
+def login():
+    if request.method == "POST":
+        nome = request.form.get("nome", "")#Faço o get no form pelo name "nome"
+        idade = request.form.get("idade", "")#Faço o get no form pelo name "idade"
+        if nome != "" and idade != "":
+            with open(JSON, "r", encoding="utf-8") as f:
+                usuarios = json.load(f)
+
+            for i in usuarios:
+                if i["nome"] == nome and i["idade"] == idade:
+                    return redirect(url_for("home", nome=nome, idade=idade))
+            
+            return redirect(url_for("login"))
+
+    return render_template("login.html")
 
 @app.route("/home")
 def home():
@@ -59,9 +73,10 @@ def home():
     nome = request.args.get("nome", "")#Pego o valor da variável nome via URL
     idade = request.args.get("idade", "")#Pego o valor da variável idade via URL
     if not nome or not idade or not idade.isdigit():
-        return redirect(url_for("register"))
+        return redirect(url_for("login"))
     else:
         return render_template("home.html", nome=nome, idade=idade)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
